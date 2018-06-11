2225f53ff3215acae28ff34401402ce9a7365993
rohithrajasekharan <rohithrajasekharan@outlook.com>
2018-06-11 21:21:50 +0530

    error fix in url

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
     maxAge: 24 * 60 * 60 * 1000,
-    keys: [process.en.keys]
+    keys: [process.env.keys]
 }));
 
 app.use(passport.initialize());
@@ -35,7 +35,7 @@ app.use(express.static('public'));
 app.use('/auth', authRoutes);
 app.use('/article', articleRoutes);
 
-mongoose.connect(process.en.dbUrl, () => {
+mongoose.connect(process.env.dbUrl, () => {
     console.log("connected to db");
 });
 
