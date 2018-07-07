e3e75686d5043527a7b4836b6d976e3d57d63793
vijaicv <vijaicv@gmail.com>
2018-07-07 12:46:30 +0530

    notifications

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const socket = require('socket.io');
 const SocketManager = require('./socketmanager');
+const Article = require('./models/article-model');
+const Question= require('./models/question-model');
+const User = require('./models/user-model');
+const Notif = require('./models/notification-model');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -19,10 +23,10 @@ app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieSession({
-    maxAge: 24 * 60 * 60 * 1000,
-    keys: [process.env.keys]
-}));
+// app.use(cookieSession({
+//     maxAge: 24 * 60 * 60 * 1000,
+//     keys: [process.env.keys]
+// }));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -32,7 +36,7 @@ app.use(express.static('public'));
 app.use('/auth', authRoutes);
 app.use('/article', articleRoutes);
 
-mongoose.connect(process.env.dbUrl, () => {
+mongoose.connect("mongodb://vijaicv:ucuredme@ds113179.mlab.com:13179/youcuredme", () => {
     console.log("connected to db");
 });
 
@@ -41,7 +45,90 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }));
 
-const server = app.listen(PORT, () => {
+app.get('/feed',(req,res)=>{
+
+//change this use populate or something!
+
+
+
+    // var uid=req.query.uid;
+    // var articles=[];
+    // Article.find({pageName:"Diabetes"}).select({likedby:false}).limit(10).exec((err,result)=>{
+    //     result.forEach(article => {
+    //         Article.find({likedby:uid,_id:article._id}).select({_id:true}).then((result2)=>{
+    //             console.log(result2.length);
+    //             var r=article.toObject()
+    //             if(result2.length==1)r.isliked=true
+    //             else r.isliked=false
+    //             User.find({_id:uid}).select({_id:0,name:1,avatar:1}).then((result)=>{
+    //                 console.log(result)
+    //                 r.author=result[0].name;
+    //                 r.avatar=result[0].avatar;
+    //                 articles.push(r);
+    //             })
+    //         })
+    //     })
+    //     Question.find({pageName:"Diabetes"}).limit(10).exec((err,result3)=>{
+    //         var response={
+    //             articles:articles,
+    //             questions:result3
+    //         }
+    //         res.json(response)
+    //     }) 
+    // })   
+})
+
+app.get('/like',(req,res)=>{
+    var aId=req.query.aid;
+    var uid=req.query.uid;
+    var iod=req.query.iod;
+
+    console.log("aid="+aId+"uid= "+uid+"iod= "+iod);
+
+    if(iod=="up"){
+        Article.findOneAndUpdate({_id :aId}, {$inc : {'likes' : 1}, $push: { 'likedby': uid } }).then((data)=>{
+            console.log("adding like")
+            Article.likedby.push(uid)
+            res.send("sucess");
+        })
+    }
+    else{
+        Article.findOneAndUpdate({_id :aId}, {$inc : {'likes' : -1}, $push: { 'likedby': uid } }).then((data)=>{
+            console.log("removing like")
+            Article.likedby.pull(uid);
+            res.send("sucess");
+        })
+    }
+    
+})
+
+
+app.get("/notification",(req,res)=>{
+    var uid=req.query.uid;
+    var dpos=req.query.dpos;//dismissPosition
+
+    console.log(dpos)
+    if(dpos=="load"){
+        Notif.find({userid:uid}).exec((err,result)=>{
+            res.json(result)
+        })
+    }
+    else if(dpos=="All"){
+        Notif.deleteMany({userid:uid}).exec((err,result)=>{
+            if(err)res.send("failed")
+            else res.send("success")
+        })
+    }
+    else {
+        Notif.findByIdAndDelete(dpos,(err,result)=>{
+            if(err)res.send("failed")
+            else res.send("success")
+        })
+    }
+})
+
+const server = app.listen(PORT, (err) => {
+    if(err)throw err;
   console.log('app listening on port '+PORT);
 })
 const io = module.exports.io = socket(server);
