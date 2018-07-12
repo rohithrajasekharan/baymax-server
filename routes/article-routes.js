ae910b21faae10c976e8a0e8edf5cdae9a71c533
rohithrajasekharan <rohithrajasekharan@outlook.com>
2018-07-12 20:13:42 +0530

    bug fix

--- routes/article-routes.js
+++ routes/article-routes.js
@@ -5,39 +5,30 @@ const Answer = require('../models/answer-model');
 
 router.post('/new', (req, res) => {
   let newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content,
-    pageName: req.body.pageName,
-    videoId: req.body.videoId,
-    imageId: req.body.imageId,
-    type: req.body.type,
-    createdAt: Date.now(),
-    likes: 0,
-    comments: 0 
+     title : req.body.title,
+     content : req.body.content,
+     userId : req.body.userId,
+     type : req.body.type,
+     videoId : req.body.videoId,
+     imageId : req.body.imageId,
+     likes: 0,
+     comments: 0,
+     createdAt: Date.now()
   });
-Article.createArticle(newArticle, (err, article) => {
-    res.json(article);
-  });
-});
-router.post('/addanswer', (req, res) => {
-  let newAnswer = new Answer({
-    answer: req.body.answer,
-    userId: req.body.userId,
-    articleId: req.body.articleId,
-    type: req.body.type,
-    createdAt: Date.now(),
-    votes: 0,
-  });
-answer.save().then(answer=>{
-  res.json(answer)
-})
-});
-
-router.get('/answers', (req,res) => {
-  Answer.find("articleId":req.params.id, (err, answers)=>{
+  newArticle.save().then((article)=>{
     res.json(article);
   })
 });
+router.get('/feed', (req, res) => {
+Answer.find({pageName:req.params.pageName}).limit(10).then((err,articles)=>{
+  res.json(articles);
+});
+});
+router.get('/answers', (req, res) => {
+Answer.find({articleId:req.params.id}, (err, article) => {
+        res.json(article);
+    });
+  });
 
 router.get('/:id', (req,res) => {
   Article.findById(req.params.id, (err, article)=>{
