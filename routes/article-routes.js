1f4fac0f3bd3051fa9ca573a773a6a91132ac107
vijaicv <vijaicv@gmail.com>
2018-07-13 02:02:06 +0530

    removed is doc

--- routes/article-routes.js
+++ routes/article-routes.js
@@ -93,18 +93,18 @@ router.get('/bookmarks/:id',(req,res)=>{
 })
 
 router.get('/feed/:pageName', (req, res) => {
-Article.find({'pageName': req.params.pageName},{title: 1, content: 1 ,type:1,videoId:1,imageId:1,likes:1,comments:1,createdAt:1}).sort({_id:-1}).limit(10).populate({path: 'userId',select: '_id name avatar isDoc'}).then((articles)=>{
+Article.find({'pageName': req.params.pageName},{title: 1, content: 1 ,type:1,videoId:1,imageId:1,likes:1,comments:1,createdAt:1}).sort({_id:-1}).limit(10).populate({path: 'userId',select: '_id name avatar'}).then((articles)=>{
   res.json(articles);
 });
 });
 router.get('/answers/:id', (req, res) => {
-Answer.find({'articleId':req.params.id}).populate({path: 'userId',select: '_id name avatar isDoc'}).then((answers)=>{
+Answer.find({'articleId':req.params.id}).populate({path: 'userId',select: '_id name avatar'}).then((answers)=>{
   res.json(answers);
 });
   });
 
 router.get('/:id', (req,res) => {
-  Article.findById(req.params.id).populate({path:'userId',select: '_id name avatar isDoc'}).then((article)=>{
+  Article.findById(req.params.id).populate({path:'userId',select: '_id name avatar'}).then((article)=>{
     res.json(article);
   });
 });
