c6346bccbf8f1831541126afaecdb5ea84a12b9c
rohithrajasekharan <rohithrajasekharan@outlook.com>
2018-07-16 20:36:00 +0530

    checking cors again

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
   credentials: true,
-  origin: "http://localhost:3000"
+  origin: "*"
 }));
 
 app.use(cookieSession({
