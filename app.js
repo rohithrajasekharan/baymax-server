c97c8b171163a3a0fd09d5c1aae359d60309e822
rohithrajasekharan <rohithrajasekharan@outlook.com>
2018-07-16 20:33:04 +0530

    cors enabled only for localhost

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
   credentials: true,
- origin: "*"
+  origin: "http://localhost:3000"
 }));
 
 app.use(cookieSession({
