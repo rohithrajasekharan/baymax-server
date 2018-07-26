04bc5d149cab447b8db9c1aa6cc88f0b6cc5b9c0
rohithrajasekharan <rohithrajasekharan@outlook.com>
2018-07-26 18:38:12 +0530

    changed formatof events

--- socketmanager.js
+++ socketmanager.js
@@ -6,59 +6,45 @@ module.exports = (ws)=> {
   const User = require('./models/user-model');
   const wss = require('./app.js').wss;
   console.log('connection is made');
-  ws.on('chat Diacare', (data)=> {
-    let newMessage = new DiabetesMessage({
-      message: data.message,
-      userId: data.userId,
-      type: data.type,
-      replyto: data.replyto,
-      createdAt: new Date()
+  ws.on('message', (raw)=> {
+    var args = JSON.parse(raw);
+    if (args.pageName=="Diacare") {
+      let newMessage = new DiabetesMessage({
+        message: args.data.message,
+        userId: args.data.userId,
+        type: args.data.type,
+        replyto: args.data.replyto,
+        createdAt: new Date()
+      });
+      newMessage.save().then((resp)=>{
+        DiabetesMessage.find({_id:resp.id}).populate({path: 'userId',select: '_id name isDoc'}).then((message) => {
+          wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send('chat Diacare',message);
+      }
     });
-    newMessage.save().then((resp)=>{
-      DiabetesMessage.find({_id:resp.id}).populate({path: 'userId',select: '_id name isDoc'}).then((message) => {
-        wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send('chat Diacare',message);
-    }
-  });
+        })
       })
-
-
-
-    })
+    }
+    else if (args.pageName=="Babyandme") {
+      let newMessage = new BabyandmeMessage({
+        message: data.message,
+        userId: data.userId,
+        type: data.type,
+        replyto: data.replyto,
+        createdAt: new Date()
+      });
+      newMessage.save().then((resp)=>{
+        BabyandmeMessage.find({_id:resp.id}).populate({path: 'userId',select: '_id name isDoc'}).then((message) => {
+          wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send('chat Babyandme',message);
+      }
+    });
+        })
+      })
+    }
 
   })
-  ws.on('chat Babyandme', (data)=> {
-    let newMessage = new BabyandmeMessage({
-      message: data.message,
-      userId: data.userId,
-      type: data.type,
-      replyto: data.replyto,
-      createdAt: new Date()
-    });
-    newMessage.save().then((resp)=>{
-      BabyandmeMessage.find({_id:resp.id}).populate({path: 'userId',select: '_id name isDoc'}).then((message) => {
-        wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send('chat Babyandme',message);
-    }
-  });
-      })
-
-
-
-    })
-
-  })
-
-  ws.on('typing', function(user){
-    console.log(user+" is typing");
-    wss.clients.forEach(function each(client) {
-if (client.readyState === WebSocket.OPEN) {
-  client.send('typing',user);
-}
-});
-  });
-
 
 };
