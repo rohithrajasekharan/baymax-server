fdd7be4d0975d5c3032f8c6fd881bd50435df5c2
rohithrajasekharan <rohithrajasekharan@outlook.com>
2018-07-27 10:29:27 +0530

    fixed typos in chat-route

--- routes/chat-routes.js
+++ routes/chat-routes.js
@@ -9,20 +9,32 @@ const Schema = mongoose.Schema;
 
 router.post('/load',(req,res)=>{
   if (req.body.pageName==="Diabetes" && req.body.id=="") {
-      return DiabetesMessage.find().limit(50).populate({path: 'userId',select: '_id name isDoc'}).sort({_id:-1});
+      DiabetesMessage.find().limit(50).populate({path: 'userId',select: '_id name isDoc'}).sort({_id:-1}).then((resp)=>{
+        res.json(resp);
+      });
   }else if (req.body.pageName==="Baby and Me" && req.body.id==""){
-      return BabyandmeMessage.find().limit(50).populate({path: 'userId',select: '_id name isDoc'}).sort({_id:-1});
+      BabyandmeMessage.find().limit(50).populate({path: 'userId',select: '_id name isDoc'}).sort({_id:-1}).then((resp)=>{
+        res.json(resp);
+      });
   }else if (req.body.pageName==="Diabetes" && req.body.id!=="") {
-      return DiabetesMessage.find({_id: {$lt: req.body.id}}).limit(50).populate({path: 'userId',select: '_id name isDoc'}).sort({_id:-1});
+      DiabetesMessage.find({_id: {$lt: req.body.id}}).limit(50).populate({path: 'userId',select: '_id name isDoc'}).sort({_id:-1}).then((resp)=>{
+        res.json(resp);
+      });
   }else {
-      return BabyandmeMessage.find({_id: {$lt: req.body.id}}).limit(50).populate({path: 'userId',select: '_id name isDoc'}).sort({_id:-1});
+      BabyandmeMessage.find({_id: {$lt: req.body.id}}).limit(50).populate({path: 'userId',select: '_id name isDoc'}).sort({_id:-1}).then((resp)=>{
+        res.json(resp);
+      });
   }
 })
 router.get('/new/:pageName/:id',(req,res)=>{
   if (req.params.pageName=="Diabetes") {
-      return DiabetesMessage.find({_id: {$gt: req.params.id}}).limit(1).count();
+      DiabetesMessage.find({_id: {$gt: req.params.id}}).limit(1).count().then((resp)=>{
+        res.json(resp);
+      });
   }else if (req.params.pageName=="babyandme"){
-      return BabyandmeMessage.find({_id: {$gt: req.params.id}}).limit(1).count();
+      BabyandmeMessage.find({_id: {$gt: req.params.id}}).limit(1).count().then((resp)=>{
+        res.json(resp);
+      });
   }
 })
 
