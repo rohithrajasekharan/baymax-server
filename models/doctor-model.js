c6aa851e038dd315fdf9b37f6563c6d62d7eb74e
rohithrajasekharan <rohithrajasekharan@outlook.com>
2018-11-17 08:23:43 +0530

    fixed bug

--- models/doctor-model.js
+++ models/doctor-model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const Hospital = require('../hospital-model');
+const Hospital = require('./hospital-model');
+const Schema = mongoose.Schema;
 
 const doctorSchema = mongoose.Schema({
   name: String,
